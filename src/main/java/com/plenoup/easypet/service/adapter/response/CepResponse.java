a68943f161cb11008a8cfa361996949cf627a537
Victor Lucio <Victorbcl@gmail.com>
2022-06-08 01:56:17 -0300

    wiremock usando arquivo json p response

--- src/main/java/com/plenoup/easypet/service/adapter/response/CepResponse.java
+++ src/main/java/com/plenoup/easypet/service/adapter/response/CepResponse.java
@@ -10,4 +10,13 @@ public class CepResponse {
 
     @JsonProperty("logradouro")
     private String logradouro;
+
+    @JsonProperty("bairro")
+    private String bairro;
+
+    @JsonProperty("localidade")
+    private String localidade;
+
+    @JsonProperty("uf")
+    private String uf;
 }
