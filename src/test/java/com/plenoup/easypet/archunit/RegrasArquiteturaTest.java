4d2a940736def8e4b4d96be036f2c8082dc41904
Victor Lucio <Victorbcl@gmail.com>
2022-06-08 13:03:17 -0300

    test: arch unit

---
+++ src/test/java/com/plenoup/easypet/archunit/RegrasArquiteturaTest.java
@@ -0,0 +1,36 @@
+package com.plenoup.easypet.archunit;
+
+import com.tngtech.archunit.core.importer.ImportOption;
+import com.tngtech.archunit.junit.AnalyzeClasses;
+import com.tngtech.archunit.junit.ArchTest;
+import com.tngtech.archunit.lang.ArchRule;
+import com.tngtech.archunit.lang.syntax.ArchRuleDefinition;
+import org.springframework.context.annotation.Configuration;
+
+@AnalyzeClasses(packages = "com.plenoup.easypet", importOptions = {ImportOption.DoNotIncludeTests.class})
+public class RegrasArquiteturaTest {
+
+    @ArchTest
+    static final ArchRule classes_de_implementacao_nao_devem_ser_publicas =
+            ArchRuleDefinition.classes()
+                    .that().haveNameMatching(".*Impl.*")
+                    .should().bePackagePrivate()
+                    .because("Classes de implementação não devem ser publicas, e sim acessadas pela interface");
+
+    @ArchTest
+    static final ArchRule classes_exception_devem_possuir_mensagem_padrao =
+            ArchRuleDefinition.fields()
+                    .that().areDeclaredInClassesThat().haveNameMatching(".*Exception.*")
+                    .should().bePrivate()
+                    .andShould().beStatic()
+                    .andShould().beFinal()
+                    .andShould().haveRawType(String.class)
+                    .because("Classes 'Exception' devem possuir campo de mensagem default");
+
+    @ArchTest
+    static final ArchRule classes_com_annotation_configuration_devem_possuir_sufixo_Configuration =
+            ArchRuleDefinition.classes()
+                    .that().areAnnotatedWith(Configuration.class)
+                    .should().haveNameMatching(".*Configuration")
+                    .because("Classes anotadas com '@Configuration' devem possuir 'Configuration' no sufixo");
+}
