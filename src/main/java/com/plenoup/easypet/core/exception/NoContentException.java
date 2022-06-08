6384e162fc0fabf827c15cd672e661fa93b07632
Victor Lucio <Victorbcl@gmail.com>
2022-06-08 13:02:29 -0300

    chore: ajuste add mesnagem padrao

--- src/main/java/com/plenoup/easypet/core/exception/NoContentException.java
+++ src/main/java/com/plenoup/easypet/core/exception/NoContentException.java
@@ -2,6 +2,9 @@ package com.plenoup.easypet.core.exception;
 
 public class NoContentException extends RuntimeException {
 
+    private static final String MESSAGE = "Recurso retornou resposta sem conteúdo";
+
     public NoContentException() {
+        super(MESSAGE);
     }
 }
